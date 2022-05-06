84e61745d96eaf4daeaaf768522db97952e37e7e
APPLEQIAN <861756992@qq.com>
2022-05-06 09:05:32 +0800

    hdy

--- src/include/page/disk_file_meta_page.h
+++ src/include/page/disk_file_meta_page.h
@@ -22,7 +22,7 @@ class DiskFileMetaPage {
  public:
   uint32_t num_allocated_pages_{0};
   uint32_t num_extents_{0};  // each extent consists with a bit map and BIT_MAP_SIZE pages
-  uint32_t extent_used_page_[100];
+  uint32_t extent_used_page_[0];
 };
 
 #endif  // MINISQL_DISK_FILE_META_PAGE_H
