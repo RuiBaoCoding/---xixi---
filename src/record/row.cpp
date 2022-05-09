9e4a96c0cc6609bb6c64ddbda63fdd5d8d9c5553
FionaEd <3200100992@zju.edu.cn>
2022-05-09 11:29:42 +0800

    serialize_row

--- src/record/row.cpp
+++ src/record/row.cpp
@@ -14,17 +14,22 @@ uint32_t Row::SerializeTo(char *buf, Schema *schema) const {
 
 uint32_t Row::DeserializeFrom(char *buf, Schema *schema) {
   // replace with your code here
-  auto len=schema->GetColumnCount();
-  Field *df = nullptr;
+  uint32_t len=schema->GetColumnCount();
   MemHeap *heap = new SimpleMemHeap();
   char *buffer = buf;
-  for(auto i=0;i<len;i++){ 
-    buffer+=fields_[i]->DeserializeFrom(buffer, schema->GetColumn(i)->GetType(), &fields_[i], false, heap);
+  for(int i=0;i<int(len);i++){ 
+    buffer+=fields_[i]->Field::DeserializeFrom(buffer, schema->GetColumn(i)->GetType(), &fields_[i], false, heap);
   }
   return buffer-buf;
 }
 
 uint32_t Row::GetSerializedSize(Schema *schema) const {
   // replace with your code here
-  return 0;
+  uint32_t ans=0;
+  int len = fields_.size();
+  for(int i=0;i<len;i++){
+    auto tmp = this->fields_[i];//取出row中的每个Field指针
+    ans+=tmp->GetSerializedSize();
+  }
+  return ans;
 }
