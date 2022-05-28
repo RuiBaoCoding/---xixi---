1518d12372a4a4e696f61ce78169825812e2d257
RuiBaoCoding <1022396243@qq.com>
2022-05-28 15:33:47 +0800

    main.cpp & engine

--- src/catalog/executor/execute_engine.cpp
+++ src/catalog/executor/execute_engine.cpp
@@ -1,5 +1,6 @@
 #include "executor/execute_engine.h"
 #include "glog/logging.h"
+#include <vector>
 
 ExecuteEngine::ExecuteEngine() {
 
@@ -58,69 +59,235 @@ dberr_t ExecuteEngine::ExecuteCreateDatabase(pSyntaxNode ast, ExecuteContext *co
 #ifdef ENABLE_EXECUTE_DEBUG
   LOG(INFO) << "ExecuteCreateDatabase" << std::endl;
 #endif
-  return DB_FAILED;
+  DBStorageEngine* db = new DBStorageEngine(ast->child_->val_);
+  //放入map映射里面
+  dbs_[ast->child_->val_]=db;
+  //cout<<"ExecuteCreateDatabase SUCCESS!"<<endl;
+  return DB_SUCCESS;
+  //return DB_FAILED;
 }
 
 dberr_t ExecuteEngine::ExecuteDropDatabase(pSyntaxNode ast, ExecuteContext *context) {
 #ifdef ENABLE_EXECUTE_DEBUG
   LOG(INFO) << "ExecuteDropDatabase" << std::endl;
 #endif
-  return DB_FAILED;
+  delete dbs_[ast->child_->val_];
+  dbs_.erase(ast->child_->val_);
+  return DB_SUCCESS;
+  //return DB_FAILED;
 }
 
 dberr_t ExecuteEngine::ExecuteShowDatabases(pSyntaxNode ast, ExecuteContext *context) {
 #ifdef ENABLE_EXECUTE_DEBUG
   LOG(INFO) << "ExecuteShowDatabases" << std::endl;
 #endif
-  return DB_FAILED;
+  cout<<"------DataBases------"<<endl;
+  for (auto p=dbs_.begin();p!=dbs_.end();p++){
+    std::cout<<p->first<<std::endl;
+  }
+  //cout<<"ExecuteShowDatabases SUCCESS!"<<endl;
+  return DB_SUCCESS;
+  //return DB_FAILED;
 }
 
 dberr_t ExecuteEngine::ExecuteUseDatabase(pSyntaxNode ast, ExecuteContext *context) {
 #ifdef ENABLE_EXECUTE_DEBUG
   LOG(INFO) << "ExecuteUseDatabase" << std::endl;
 #endif
-  return DB_FAILED;
+  
+  if(dbs_.count(ast->child_->val_)){
+    current_db_=ast->child_->val_;
+    current_db=dbs_[current_db_];
+  }else{
+    cout<<"Invalid DataBase Name!"<<endl;
+  }
+  return DB_SUCCESS;
+  //return DB_FAILED;
 }
 
 dberr_t ExecuteEngine::ExecuteShowTables(pSyntaxNode ast, ExecuteContext *context) {
 #ifdef ENABLE_EXECUTE_DEBUG
   LOG(INFO) << "ExecuteShowTables" << std::endl;
 #endif
-  return DB_FAILED;
+  cout<<"------Tables------"<<endl;
+  vector<TableInfo* > tables;
+  current_db->catalog_mgr_->GetTables(tables);
+  for(auto p=tables.begin();p<tables.end();p++){
+    cout<<(*p)->GetTableName()<<endl;
+  }
+  return DB_SUCCESS;
+  //return DB_FAILED;
 }
 
 dberr_t ExecuteEngine::ExecuteCreateTable(pSyntaxNode ast, ExecuteContext *context) {
 #ifdef ENABLE_EXECUTE_DEBUG
   LOG(INFO) << "ExecuteCreateTable" << std::endl;
 #endif
-  return DB_FAILED;
+  string table_name = ast->child_->val_;
+  //cout<<"table_name:"<<table_name<<endl;
+  pSyntaxNode column_pointer= ast->child_->next_->child_;//第一个属性对应的指针
+  vector<Column*>vec_col;
+  while(column_pointer!=nullptr&&column_pointer->type_==kNodeColumnDefinition){
+    //它的孩子 是表的属性信息
+    //cout<<"---------------"<<endl;
+    bool is_unique = false;
+    if (column_pointer->val_!=nullptr){
+      string s = column_pointer->val_;
+      is_unique = (s=="unique");
+    }
+    string column_name = column_pointer->child_->val_;//属性名字
+    //cout<<"column_name:"<<column_name<<endl;
+    string column_type = column_pointer->child_->next_->val_;//属性类型
+    //cout<<"column_type:"<<column_type<<endl;
+    int cnt = 0;
+    Column *now;
+    if(column_type=="int"){
+      now = new Column(column_name,kTypeInt,cnt,true,is_unique);
+    }
+    else if(column_type=="char"){
+      string len = column_pointer->child_->next_->child_->val_;
+      //cout<<"len:"<<len<<endl;
+      long unsigned int a = -1;
+      if (len.find('.')!=a){
+        cout<<"Semantic Error, String Length Can't be a Decimal!"<<endl;
+        return DB_FAILED;
+      }
+      int length = atoi(column_pointer->child_->next_->child_->val_);
+      if (length<0){
+        cout<<"Semantic Error, String Length Can't be Negative!"<<endl;
+        return DB_FAILED;
+      }
+      now = new Column(column_name,kTypeChar,length,cnt,true,is_unique);
+    }
+    else if(column_type=="float"){
+      now = new Column(column_name,kTypeFloat,cnt,true,is_unique);
+    }
+    else{
+      cout<<"Error Column Type!"<<endl;
+      return DB_FAILED;
+    }
+    //cout<<"is_unique:"<<is_unique<<endl;
+    vec_col.push_back(now);
+    column_pointer = column_pointer->next_;
+    cnt++;
+  }
+  //为primary key建立索引
+  Schema *schema = new Schema(vec_col);
+  TableInfo *table_info = nullptr;
+  //cout<<"SUCCEED!"<<endl;
+  dberr_t IsCreate=current_db->catalog_mgr_->CreateTable(table_name,schema,nullptr,table_info);
+  if(IsCreate==DB_TABLE_ALREADY_EXIST){
+    cout<<"Table Already Exist!"<<endl;
+    return IsCreate;
+  }
+  if (column_pointer!=nullptr){
+    cout<<"It has primary key!"<<endl;
+    pSyntaxNode key_pointer = column_pointer->child_;
+    vector <string>primary_keys;
+    while(key_pointer!=nullptr){
+      string key_name = key_pointer->val_ ;
+      cout<<"key_name:"<<key_name<<endl;
+      primary_keys.push_back(key_name);
+      key_pointer = key_pointer->next_;
+    }
+    CatalogManager* current_catalog=current_db->catalog_mgr_;
+    IndexInfo* indexinfo=nullptr;
+    string index_name = table_name + "_pk";
+    cout<<"index_name:"<<index_name;
+    current_catalog->CreateIndex(table_name,index_name,primary_keys,nullptr,indexinfo);
+  }
+  return IsCreate;
 }
+
 
 dberr_t ExecuteEngine::ExecuteDropTable(pSyntaxNode ast, ExecuteContext *context) {
 #ifdef ENABLE_EXECUTE_DEBUG
   LOG(INFO) << "ExecuteDropTable" << std::endl;
 #endif
-  return DB_FAILED;
+  dberr_t IsDrop=current_db->catalog_mgr_->DropTable(ast->child_->val_);
+  if(IsDrop==DB_TABLE_NOT_EXIST){
+    cout<<"Table Not Exist!"<<endl;
+  }
+  return IsDrop;
+  //return DB_FAILED;
 }
 
 dberr_t ExecuteEngine::ExecuteShowIndexes(pSyntaxNode ast, ExecuteContext *context) {
 #ifdef ENABLE_EXECUTE_DEBUG
   LOG(INFO) << "ExecuteShowIndexes" << std::endl;
 #endif
-  return DB_FAILED;
+  cout<<"------Indexes------"<<endl;
+  vector<TableInfo* > tables;
+  current_db->catalog_mgr_->GetTables(tables);
+  //先获得所有的表，然后遍历表得到每个表的索引
+  for(auto p=tables.begin();p<tables.end();p++){
+    //遍历每个表
+    cout<<"Indexes of Table "<<(*p)->GetTableName()<<":"<<endl;
+    vector<IndexInfo*> indexes;
+    current_db->catalog_mgr_->GetTableIndexes((*p)->GetTableName(),indexes);
+    for(auto q=indexes.begin();q<indexes.end();q++){
+      cout<<(*q)->GetIndexName()<<endl;
+    }
+  }
+  return DB_SUCCESS;
+  //return DB_FAILED;
 }
 
 dberr_t ExecuteEngine::ExecuteCreateIndex(pSyntaxNode ast, ExecuteContext *context) {
 #ifdef ENABLE_EXECUTE_DEBUG
   LOG(INFO) << "ExecuteCreateIndex" << std::endl;
 #endif
-  return DB_FAILED;
+  vector <string> index_keys;
+  //得到index_key的第一个结点
+  pSyntaxNode index_key=ast->child_->next_->next_->child_;
+  for(;index_key!=nullptr;index_key=index_key->next_){
+    index_keys.push_back(index_key->val_);
+  }
+  CatalogManager* current_catalog=current_db->catalog_mgr_;
+  IndexInfo* indexinfo=nullptr;
+  dberr_t IsCreate=current_catalog->CreateIndex(ast->child_->next_->val_
+    ,ast->child_->val_,index_keys,nullptr,indexinfo);
+  if(IsCreate==DB_TABLE_NOT_EXIST){
+    cout<<"Table Not Exist!"<<endl;
+  }
+  if(IsCreate==DB_INDEX_ALREADY_EXIST){
+    cout<<"Index Already Exist!"<<endl;
+  }
+  return IsCreate;
+  //return DB_FAILED;
 }
 
 dberr_t ExecuteEngine::ExecuteDropIndex(pSyntaxNode ast, ExecuteContext *context) {
 #ifdef ENABLE_EXECUTE_DEBUG
   LOG(INFO) << "ExecuteDropIndex" << std::endl;
 #endif
+  //假设index_name是不一样的，则我们找到一个后就直接返回
+  //如果有多个则函数返回值无法处理
+  vector<TableInfo* > tables;
+  current_db->catalog_mgr_->GetTables(tables);
+  //先获得所有的表，然后遍历表得到每个表的索引
+  for(auto p=tables.begin();p<tables.end();p++){
+    //遍历每个表
+    //cout<<"Indexes of Table "<<(*p)->GetTableName()<<":"<<endl;
+    vector<IndexInfo*> indexes;
+    current_db->catalog_mgr_->GetTableIndexes((*p)->GetTableName(),indexes);
+    string index_name=ast->child_->val_;
+    for(auto q=indexes.begin();q<indexes.end();q++){
+      //判断如果相同就删除
+      if((*q)->GetIndexName()==index_name){
+        dberr_t IsDrop=current_db->catalog_mgr_->DropIndex((*p)->GetTableName(),index_name);
+        if(IsDrop==DB_TABLE_NOT_EXIST){
+          cout<<"Table Not Exist!"<<endl;
+        }
+        if(IsDrop==DB_INDEX_NOT_FOUND){
+          cout<<"Index Not Found!"<<endl;
+        }
+        return IsDrop;
+      }
+    }
+  }
+  //如果到这里还没返回说明没找到，没有删除成功
+  cout<<"Index Not Found!"<<endl;
   return DB_FAILED;
 }
 
@@ -135,7 +302,92 @@ dberr_t ExecuteEngine::ExecuteInsert(pSyntaxNode ast, ExecuteContext *context) {
 #ifdef ENABLE_EXECUTE_DEBUG
   LOG(INFO) << "ExecuteInsert" << std::endl;
 #endif
-  return DB_FAILED;
+  //首先得到表名
+  string table_name=ast->child_->val_;
+  //得到tableinfo*
+  //构健一个Row
+  TableInfo *tableinfo = nullptr;
+  dberr_t GetRet = current_db->catalog_mgr_->GetTable(table_name, tableinfo);
+  if (GetRet==DB_TABLE_NOT_EXIST){
+    cout<<"Table Not Exist!"<<endl;
+    return DB_FAILED;
+  }
+  vector<Field> fields;
+  pSyntaxNode column_pointer= ast->child_->next_->child_;//头指针
+  int cnt = tableinfo->GetSchema()->GetColumnCount();
+  //cout<<"cnt:"<<cnt<<endl;
+  for ( int i = 0 ; i < cnt ; i ++ ){
+    TypeId now_type_id = tableinfo->GetSchema()->GetColumn(i)->GetType();
+    if (column_pointer==nullptr){
+      for ( int j = 0 ; j < cnt-i ; j ++ ){
+        //cout<<"has null!"<<endl;
+        Field new_field(now_type_id);
+        fields.push_back(new_field);
+      }
+      break;
+    }
+    if(column_pointer->val_==nullptr ){
+      //cout<<"has null!"<<endl;
+      Field new_field(now_type_id);
+      fields.push_back(new_field);
+    }
+    else{
+      //cout<<"a number"<<endl;
+      if (now_type_id==kTypeInt){//整数
+        int x = atoi(column_pointer->val_);
+        Field new_field (now_type_id,x);
+        fields.push_back(new_field);
+      }
+      else if(now_type_id==kTypeFloat){//浮点数
+        float f = atof(column_pointer->val_);
+        Field new_field (now_type_id,f);
+        fields.push_back(new_field);
+      }
+      else {//字符串
+        string s = column_pointer->val_;
+        Field new_field (now_type_id,column_pointer->val_,s.length(),true); 
+        fields.push_back(new_field);
+      }
+    }
+    column_pointer = column_pointer->next_;
+  }
+  if (column_pointer!=nullptr){
+    cout<<"Column Count doesn't match!"<<endl;
+    return DB_FAILED;
+  }
+  Row row(fields);//构造一个row
+  //新构建的Row对象名字为row，RowID为rowid
+  //得到表中的所有索引
+  ASSERT(tableinfo!=nullptr,"TableInfo is Null!");
+  TableHeap* tableheap=tableinfo->GetTableHeap();//得到表对应的文件堆
+  bool Is_Insert=tableheap->InsertTuple(row,nullptr);
+  if(Is_Insert==false){
+    cout<<"Insert Failed, Affects 0 Record!"<<endl;
+    return DB_FAILED;
+  }else{
+    //cout<<"RowID: "<<row.GetRowId().Get()<<endl;
+    vector <IndexInfo*> indexes;//存表中所有索引的indexinfo
+    current_db->catalog_mgr_->GetTableIndexes(table_name,indexes);
+    for(auto p=indexes.begin();p<indexes.end();p++){
+      dberr_t IsInsert=(*p)->GetIndex()->InsertEntry(row,row.GetRowId(),nullptr);
+      //cout<<"RowID: "<<row.GetRowId().Get()<<endl;
+      if(IsInsert==DB_FAILED){
+        //插入失败
+        cout<<"Insert Into Index Failed, Affects 0 Record!"<<endl;
+        //插入失败则需要撤回之前在索引中插入的记录，并且从表中删除
+        for(auto q=indexes.begin();q!=p;q++){
+          (*q)->GetIndex()->RemoveEntry(row,row.GetRowId(),nullptr);
+        }
+        tableheap->MarkDelete(row.GetRowId(),nullptr);
+        return IsInsert;
+      }else{
+        cout<<"Insert Into Index Sccess"<<endl;
+      }
+    }
+    //全部都能插进去才能成功插入
+    cout<<"Insert Success, Affects 1 Record!"<<endl;
+    return DB_SUCCESS;
+  }
 }
 
 dberr_t ExecuteEngine::ExecuteDelete(pSyntaxNode ast, ExecuteContext *context) {
